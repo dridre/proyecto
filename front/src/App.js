6f1491d0c87d9052e6fba82118fb2f3da2a0f8dd
dridre <>
2022-07-07 14:45:02 +0200

    Signed-off-by: dridre <>

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,7 @@ import BotonInicial from './components/BotonInicial';
 function App() {
   return (
     <div className="App">
-      <div className='BotonInicial'>
+      <div className='ContenedorBoton'>
         <BotonInicial />
       </div>
     </div>
