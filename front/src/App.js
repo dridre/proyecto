0cc1900b0ddb8ea884414c9f06dafe1183b1570a
dridre <>
2022-07-07 10:38:56 +0200

    Signed-off-by: dridre <>

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import BotonInicial from './components/BotonInicial';
 
 function App() {
   return (
     <div className="App">
-
+      <div className='BotonInicial'>
+        <BotonInicial />
+      </div>
     </div>
   );
 }
---
+++ front/src/components/BotonInicial.jsx
@@ -0,0 +1,11 @@
+import "../styles/BotonInicial.css"
+
+function BotonInicial (){
+    return(
+<button className="BotonInicial">
+Nuevo viaje
+</button>
+    );
+}
+
+export default BotonInicial;
